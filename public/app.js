57db59514e4f8546960196bb8ad2e25d949184ba
John Stallo <john.stallo@live.com>
2016-04-18 16:04:22 -0700

    differentiate by color

--- public/app.js
+++ public/app.js
@@ -9,11 +9,22 @@ app.controller('MainController', function($scope, $http, socket) {
     $scope.messages = [];
     $scope.sayHelloToServer = function() {
         $http.get("/api").then(function(response) {
-            $scope.messages.push(response.data);
+            $scope.messages.push(response.data + new Date());
         });
     };
     
     $scope.sayHelloToServer();
+    
+    var styles = [];
+    var colors = ["gray", "green", "red", "blue", "orange", "black"];
+    var color = 0;
+    $scope.getStyle = function(message) {
+        if (!styles[message]) {
+            styles[message] = {'color': colors[color]};
+            color = color < colors.length - 1 ? color + 1 : 0;
+        }
+        return styles[message];
+    }
 
     // $scope.messages = [];
     // socket.on('hello', function(data) {
